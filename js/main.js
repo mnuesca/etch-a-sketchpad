da64cf55d7d6629f06b42c425fb193cc254f6f11
Matthew Nuesca <matthewnuesca@gmail.com>
2017-06-28 20:10:27 -0700

    Full Implementation of project

---
+++ js/main.js
@@ -0,0 +1,100 @@
+/* Waits till document is ready */
+var color = "#000000";
+var random = false;
+var lightBrush = false;
+
+$(document).ready(function() {
+  //createCanvas();
+  $('#createCanvas').on('click', createCanvas);
+  $('#clearScreen').on("click", clearScreen);
+  $('#randomize').on("click", function() {
+    random = true;
+    lightBrush = false;
+    $('.c-lightlyBrush').css("visibility", 'hidden');
+    $('.c-original').css("visibility", 'hidden');
+    $('.c-randomize').css("visibility", 'visible');
+  });
+  $('#lightlyBrush').on("click", function() {
+    lightBrush = true;
+    random = false;
+    $('.c-lightlyBrush').css("visibility", 'visible');
+    $('.c-randomize').css("visibility", 'hidden');
+    $('.c-original').css("visibility", 'hidden');
+  });
+  $('#original').on("click", function() {
+    lightBrush = false;
+    random = false;
+    $('.c-original').css("visibility", 'visible');
+    $('.c-randomize').css("visibility", 'hidden');
+    $('.c-lightlyBrush').css("visibility", 'hidden');
+  });
+
+
+});
+
+
+function clearScreen() {
+  $('.cell').each(function() {
+    $(this).removeClass("color");
+    $(this).css("background-color", "rgba(255,255,255)");
+  });
+}
+
+function changeColor() {
+  color = $('#background').val();
+  $('.color').css('background-color', color);
+}
+
+function createCanvas() {
+  $('.row').remove();
+  var result = window.prompt("How big do you want your canvas to be? Range from 16 to 100", "16");
+  if (!Number.isInteger(result) && (result < 0 || result > 100)) {
+    alert("Invalid dimensions, creating default canvas");
+    result = 16;
+  }
+  var size = 598 / result;
+  for (var k = 0; k < result; k++) {
+    $('.container').append('<div class="row"></div>');
+  }
+  $('.row').each(function() {
+    for (var i = 0; i < result; i++)
+      $(this).append('<div class="cell"></div>');
+  });
+  $('.cell').width(size);
+  $('.row').height(size);
+
+  $('input#background').change(changeColor);
+
+  $('.cell').hover(function() {
+    if (lightBrush) {
+      $(this).css("background-color", makeLighter($(this).css("background-color")));
+    } else if (random) {
+      $(this).css("background-color", randomizeColor());
+    } else {
+      $(this).css("background-color", color);
+    }
+    $(this).addClass("color");
+  });
+}
+
+function makeLighter(color) {
+  if (color === "rgb(0, 0, 0)") {
+    console.log("BLACK");
+    return;
+  }
+  color = color.replace(/[^\d,]/g, '').split(',');
+  var red = (Math.floor(color[0] - 25.5)).toString(16);
+  var green = (Math.floor(color[1] - 25.5)).toString(16);
+  var blue = (Math.floor(color[2] - 25.5)).toString(16);
+  var lighter_color = "#" + red + green + blue;
+  return lighter_color;
+
+}
+
+function randomizeColor() {
+  var red = Math.floor((Math.random() * 255) + 1).toString(16);
+  var green = Math.floor((Math.random() * 255) + 1).toString(16);
+  var blue = Math.floor((Math.random() * 255) + 1).toString(16);
+  var color = "#" + red + green;
+  return color;
+}
